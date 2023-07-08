2e7bd77852aad6b8d8c65641ad6f37f8e99f777b
mahdi moghadam <marjani.mahdi@gmail.com>
2023-07-08 14:33:03 +0330

    confirm new company

--- component/category/admin/model/CategoryCompany.model.php
+++ component/category/admin/model/CategoryCompany.model.php
@@ -24,10 +24,16 @@ class CategoryCompany extends looeic
             ->get();
 
         $resultCategory = $resultCategory['export']['list'];
-        if (count($category_id ) == 0) {
-            $sql = "delete from category_company where company_id = " . $this->company->Company_id  ;
-            CategoryCompany::query($sql)->getlist();
+        if (count($category_id) == 0) {
+            try {
+                $sql = "delete from category_company where company_id = " . $this->company->Company_id;
+                CategoryCompany::query($sql)->getlist();
+            } catch (PDOException $e) {
+                get_caller(__FUNCTION__);
 
+                echo "Error: " . $e->getMessage();
+                dd('');
+            }
         } else {
             foreach ($resultCategory as $category) {
                 if (in_array($category->category_id, $category_id)) {
